a3ae5dbf05397a273a7046d4c4820324d5c0aa2a
eftalozturk <eftal_989@outlook.com>
2022-06-05 17:32:33 +0300

    tab yapisi kuruldu

--- JS/_assets/js/javascript.js
+++ JS/_assets/js/javascript.js
@@ -1,1 +1,25 @@
-asd
+const btnSelect = document.querySelectorAll(".btn-tabs > button");
+const text = document.querySelectorAll(".text");
+btnSelect.forEach((tab, index) => {
+//   if (kural = true) {
+//       return
+//   }
+  tab.addEventListener("click", () => {
+    text.forEach((text) => {
+      text.classList.remove("is-active");
+    });
+    btnSelect.forEach((tab) => {
+      tab.classList.remove("is-active");
+    });
+    setActive(index)
+  });
+});
+
+function setActive(index) {
+  text[index].classList.add("is-active");
+  btnSelect[index].classList.add("is-active");
+}
+
+window.addEventListener("load", () => {
+    setActive(0)
+});
